c1d094dc39c96f3dafefa8e85f3ce73b6cb9c731
cuongndph18057 <cuongksv10@gmail.com>
2022-05-28 20:44:59 +0700

    adminlayout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './layout/admin/admin.component';
+import { ClientComponent } from './layout/client/client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: ClientComponent,
+  },
+  {
+    path: "admin", component: AdminComponent,
+    children: [
+      // { path:, component },
+      // { path:, component }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './layout/admin/admin.component';
+import { ClientComponent } from './layout/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
